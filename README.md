f84d5f169c78a3831f4eb5d13d140735b9826300
scott <scott@186k.group>
2023-11-20 09:49:39 -0600

    updates

--- README.md
+++ README.md
@@ -2,16 +2,11 @@
 
 ## Part 1: Intro to web3 and Blockchain
 
-### Introduction
-
-### web3 & Blockchain Primer
+(Slides to be posted here) post workshop.
 
 ## Part 2: Hands-On Demos
 
-### Demo 0: Decentralized Storage
+Building an unstoppable, simple web dApp using popular web2 technologies. If you want to run the dApp locally, simply run `python app.py` in your terminal. You may need a couple dependencies that you can install with `pip install -r requirements.txt`. 
 
- - Storj
+This web dApp pulls the latest Ethereum price using a decentralized RPC call ([Pokt](https://docs.pokt.network/get-rpcs/find-a-gateway/grove)) to a decentralized price feed ([Chainlink](https://blog.chain.link/chainlink-price-feeds-secure-defi/)). It also allows the user to download and render an image of an NFT (that was used as a [POAP](https://poap.gallery/event/159127) for this workshop) from the decentralized stoarge provider Storj.
 
-### Demo 1: 
-
-### Demo 2: Minting a POAP
--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 def get_eth_price():
     # Change this to use your own RPC URL
-    web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth_goerli'))
+    # web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth_goerli'))
+    web3 = Web3(Web3.HTTPProvider('https://eth-goerli.gateway.pokt.network/v1/lb/961ce20a'))
     # AggregatorV3Interface ABI
     abi = '[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
     # Price Feed address
